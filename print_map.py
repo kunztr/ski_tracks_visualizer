8eb58fa58904c9c30c508c902c055f8e7a546148
Tyler Kunz <tyler.kunz@aggiemail.usu.edu>
2021-04-03 20:00:17 -0600

    Added readme and comments

--- print_map.py
+++ print_map.py
@@ -3,6 +3,7 @@ import gpxpy
 from os import listdir
 
 
+# Generates a list of coordinates from a gpx file
 def open_gpx(file):
     gpx_file = open(file, "r")
     gpx = gpxpy.parse(gpx_file)
@@ -16,6 +17,7 @@ def open_gpx(file):
     return coordinates
 
 
+# prints all gpx files in the 2021-21 directory. The "year" variable can be changed to any year directory
 def main():
     ski_map = folium.Map()
     year = "2020-21"
@@ -26,6 +28,7 @@ def main():
     ski_map.save("maps/ski_tracks_" + year + ".html")
 
 
+# prints all gpx files in gpx_file directory
 def print_all():
     increment = color_increment()
     color_count = increment
@@ -36,13 +39,14 @@ def print_all():
             coordinates = open_gpx("gpx_files/" + directory + "/" + file)
             hex_color = hex(color_count)
             print(hex_color[2:len(hex_color)])
-            #folium.Marker(coordinates[0], popup=directory + ": " + file, icon=folium.Icon(color="red")).add_to(ski_map)
+            # folium.Marker(coordinates[0], popup=directory + ": " + file, icon=folium.Icon(color="red")).add_to(ski_map)
 
-            folium.PolyLine(coordinates, color="#" + hex_color[2:len(hex_color)] + "ff", weight=2, opacity=1).add_to(ski_map)
+            folium.PolyLine(coordinates, color="#" + hex_color[2:len(hex_color)], weight=2, opacity=1).add_to(ski_map)
             color_count += increment
     ski_map.save("maps/ski_tracks_all_blue_no_markers.html")
 
 
+# Determines color shift depending on the number of gpx files
 def color_increment():
     count = 2
     for directory in listdir("gpx_files"):
@@ -55,25 +59,13 @@ def color_increment():
 
     return int(increment)
 
-def color_increment_red():
-    count = 2
-    for directory in listdir("gpx_files"):
-        for file in listdir("gpx_files/" + directory):
-            count += 1
-
-    print(count)
-    max_hex = 65535
-    increment = max_hex / count
-
-    return int(increment)
 
 
 
 
 
 
+# print_all()
 
-print_all()
-
-#if __name__ == "__main__":
-    #main()
+if __name__ == "__main__":
+    main()
---
+++ readme.md
@@ -0,0 +1,12 @@
+### Ski Tracks Visualizer
+
+This is a simple script that takes gpx files and displays them on an html map using Folium.
+
+GPX files were generated using an app called Ski Tracks.
+
+The main function prints all gpx files in the 2020-21 directory.
+
+The print_all function prints all gpx files in the gpx_files directory.
+
+
+
